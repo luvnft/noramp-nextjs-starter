29adca95bc267f95b780b85ea16345824bfd70c4
The Wizard of Hahz <info@blkluv.org>
2024-02-01 20:29:39 -0500

    updated

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { LNE } from "./webclip.png";
+import { LNE } from '/public/webclip.png';
 import { Button } from "./ui/button";
 import {
   DropdownMenu,
